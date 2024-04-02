dea4c490b2a69627a002d5249a7e0e12f53efad9
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-02 14:16:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ with tab2:
     st.write('Por favor, carregue a imagem para obter a identificação')
 
     with st.spinner('Por favor, aguarde enquanto analisamos a sua imagem'):
-        yolo = YOLO_Pred(onnx_model='./best_yolo5n.onnx',
+        yolo = YOLO_Pred(onnx_model='./best.onnx',
                          data_yaml='./data.yaml')
         #st.balloons()
 
