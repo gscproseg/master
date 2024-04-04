7e2cb31429524cda90c2626f17aa22256fc62b7a
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-04 20:25:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -145,25 +145,18 @@ with tab3:
     import streamlit as st 
     from streamlit_webrtc import webrtc_streamer
     import av
-    from yolo_predictions import YOLO_Pred
-    
-    # load yolo model
-    yolo = YOLO_Pred('./models/best.onnx',
-                     './models/data.yaml')
-    
-    
+       
     def video_frame_callback(frame):
         img = frame.to_ndarray(format="bgr24")
         # any operation 
-        #flipped = img[::-1,:,:]
         pred_img = yolo.predictions(img)
     
-        return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
     
     
     webrtc_streamer(key="example", 
                     video_frame_callback=video_frame_callback,
-                    media_stream_constraints={"video":True,"audio":False})
+                    #media_stream_constraints={"video":True,"audio":False})
 
 #    import av
 #    from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
