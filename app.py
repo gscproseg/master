1a648372bb26bb977db7faa5a2e2a724f959bab1
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-08 00:10:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -147,22 +147,24 @@ with tab3:
     import av
     from yolo_predictions import YOLO_Pred
     
-    # load yolo model
-    yolo = YOLO_Pred
-    
+    # Carregue o modelo YOLO
+    yolo = YOLO_Pred()
     
     def video_frame_callback(frame):
         img = frame.to_ndarray(format="bgr24")
-        # any operation 
-        #flipped = img[::-1,:,:]
-        pred_img = yolo.predictions(img)
-    
+        # Qualquer operação que você deseja aplicar ao quadro
+        # Por exemplo, inverter horizontalmente:
+        flipped = img[:, ::-1, :]
+        # Em seguida, faça previsões com o modelo YOLO
+        pred_img = yolo.predictions(flipped)
         return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
     
-    
-    webrtc_streamer(key="example", 
-                    video_frame_callback=video_frame_callback,
-                    media_stream_constraints={"video":True,"audio":False})
+    webrtc_streamer(
+        key="example",
+        video_frame_callback=video_frame_callback,
+        media_stream_constraints={"video": True, "audio": False}
+    )
+
 
 
 
