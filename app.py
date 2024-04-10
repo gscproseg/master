4a300fc00be0543ae910cd58b5891a9cf8b06e6c
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-09 23:48:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ with tab3:
     from yolo_predictions import YOLO_Pred
     
     # Carregue o modelo YOLO
-    yolocam = YOLO_Pred(onnx_model='./best.onnx', data_yaml='./data.yaml')
+    yolo = YOLO_Pred(onnx_model = './best.onnx', data_yaml = './data.yaml')
     
     # Definir configuração RTC (WebRTC)
     rtc_configuration = RTCConfiguration(
