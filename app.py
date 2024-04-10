ee4d8b8542ee1cf3298d2d5c114dce643d1df067
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-10 00:13:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,18 +143,17 @@ pass
 #################################
 with tab3:
 
-    import streamlit as st 
+    import streamlit as st
     from streamlit_webrtc import (
         VideoProcessorBase,
         RTCConfiguration,
         webrtc_streamer
     )
     import av
-    import cv2
     from yolo_predictions import YOLO_Pred
     
     # Carregar o modelo YOLO
-    yolocam = YOLO_Pred(onnx_model='./best.onnx', data_yaml='./data.yaml')
+    #yolocam = YOLO_Pred(onnx_model='./best.onnx', data_yaml='./data.yaml')
     
     # Definir configuração RTC (WebRTC)
     rtc_configuration = RTCConfiguration(
@@ -164,7 +163,7 @@ with tab3:
     class YOLOVideoProcessor(VideoProcessorBase):
         async def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
             img_cam = frame.to_ndarray(format="bgr24")
-            pred_img_video = yolocam.predictions(img_cam)
+            pred_img_video = yolo.predictions(img_cam)
     
             # Adicionar mensagem de log para verificar as previsões
             st.write(f"Previsões: {pred_img_video}")
@@ -172,19 +171,19 @@ with tab3:
             return av.VideoFrame.from_ndarray(pred_img_video, format="bgr24")
     
     # Configurar e iniciar a transmissão WebRTC
-    with st.spinner('Aguardando a transmissão de vídeo começar...'):
-        webrtc_ctx = webrtc_streamer(
-            key="example",
-            video_processor_factory=YOLOVideoProcessor,
-            rtc_configuration=rtc_configuration,
-            async_processing=True,
-            media_stream_constraints={"video": True, "audio": False},
-        )
+    webrtc_ctx = webrtc_streamer(
+        key="example",
+        video_processor_factory=YOLOVideoProcessor,
+        rtc_configuration=rtc_configuration,
+        async_processing=True,
+        media_stream_constraints={"video": True, "audio": False},
+    )
     
     # Exibir a interface do Streamlit
     if webrtc_ctx.state == "running":
         st.write("Streaming de vídeo com detecção de objetos está ativo.")
     else:
         st.write("Aguardando a transmissão de vídeo começar...")
+
 
 
