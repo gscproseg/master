01c5d596f856439bfcee0f824cb2cb570924b66f
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-09 23:58:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,8 +152,8 @@ with tab3:
     import av
     from yolo_predictions import YOLO_Pred
     
-    # Carregue o modelo YOLO
-    yolo = YOLO_Pred(onnx_model = './best.onnx', data_yaml = './data.yaml')
+    # Carregar o modelo YOLO
+    yolocam = YOLO_Pred(onnx_model='./best.onnx', data_yaml='./data.yaml')
     
     # Definir configuração RTC (WebRTC)
     rtc_configuration = RTCConfiguration(
@@ -164,6 +164,10 @@ with tab3:
         def transform(self, frame: av.VideoFrame) -> av.VideoFrame:
             img_cam = frame.to_ndarray(format="bgr24")
             pred_img_video = yolocam.predictions(img_cam)
+    
+            # Adicionar mensagem de log para verificar as previsões
+            st.write(f"Previsões: {pred_img_video}")
+    
             return av.VideoFrame.from_ndarray(pred_img_video, format="bgr24")
     
     # Configurar e iniciar a transmissão WebRTC
@@ -172,7 +176,7 @@ with tab3:
             key="example",
             video_processor_factory=YOLOVideoTransformer,
             rtc_configuration=rtc_configuration,
-            async_processing=True,  # Use async_processing em vez de async_transform
+            async_processing=True,
             media_stream_constraints={"video": True, "audio": False},
         )
     
@@ -181,3 +185,4 @@ with tab3:
         st.write("Streaming de vídeo com detecção de objetos está ativo.")
     else:
         st.write("Aguardando a transmissão de vídeo começar...")
+
