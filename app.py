5e1be378a56f96fafade8034db0ecc1ad0e230e2
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-06-24 23:59:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def main():
             st.image(pred_img_obj)
 
 if __name__ == "__main__":
-    yolo = YOLO_Pred(onnx_model='./best.onnx', data
+    yolo = YOLO_Pred(onnx_model='./best.onnx', './data.yaml'
