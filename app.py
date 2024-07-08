9275e8701b9014257ccbe19b6909ad985b10b64b
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-07-08 20:10:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,56 +11,14 @@ st.set_page_config(
 )
 
 # Criação das guias
-tab1, tab2, tab3 = st.tabs(["Home", "MLens - Image", "MLens - Video"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["MLens", "Detecção em Imagens", "Detecção em Vídeo", "Tempo Real - USB", "Quem somos"])
 
 # Conteúdo da página "Home"
 with tab1:
-    st.subheader("| A Classe Myxozoa")
-    # Use uma única coluna para posicionar a imagem e o texto na mesma linha
-    col1, col2 = st.columns([1,0.85])  # Defina a largura da primeira coluna
 
-    with col1:
-        # Adicione a imagem ao espaço em branco
-        st.image("./images/sera.png", width=638)
-        # Adicione a legenda da imagem
-        st.caption("""Courtesy W.L. Current
-                   Myxobolus/Myxosoma sp.
-                   """, unsafe_allow_html=True)  
-        # Adicione um espaçamento para criar espaço entre a imagem e o texto
-        st.text("")  # Ajuste o espaço conforme necessário
-
-    with col2:
-        # Ajuste a largura da coluna 2 (texto)
-        st.markdown(""*20)  # Isso cria um espaço em branco para ajustar a largura
-        intro_text = """
-        Os myxozoários são parasitas com ciclos de vida complexos, pertencentes ao filo Cnidaria, como águas-vivas e medusas.
-        Com mais de 65 gêneros e 2.200 espécies, a maioria parasita peixes, causando doenças graves e alta mortalidade.
-        Myxobolus é o gênero mais conhecido, especialmente a espécie Myxobolus cerebralis, responsável pela "Doença do rodopio"
-        em salmonídeos e danos à aquicultura e populações de peixes selvagens. Outros gêneros notáveis são Henneguya, Kudoa
-        e Ellipsomyxa. Alguns myxozoários já foram relatados em humanos, causando surtos após o consumo de peixe cru infectado
-        no Japão. O ciclo de vida envolve hospedeiros intermediários (peixes) e definitivos (anelídeos). Apesar da importância
-        zoonótica, esses parasitas não são inspecionados no pescado brasileiro, ao contrário dos Estados Unidos. A abordagem 
-        da Saúde Única promove a saúde sustentável de pessoas, animais e ecossistemas, reconhecendo sua interdependência e
-        envolvendo vários setores para enfrentar ameaças à saúde, ecossistemas, segurança alimentar e mudanças climáticas,
-        contribuindo para o desenvolvimento sustentável.
-        """
-        #st.markdown(intro_text)
-        
-        st.write(f'<p style="color:#9c9d9f">{intro_text}</p>', unsafe_allow_html=True)
-        audio_file = open("images/p_9841290_826.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mpeg")
-
-        st.subheader("| Seu ciclo de vida")
-        st.write(
-            '<p style="color:#9c9d9f">Seu ciclo de vida é indireto, envolvendo hospedeiros intermediários (peixes) e definitivos (anelídeos)</p>',
-            unsafe_allow_html=True,
-            )
-        st.subheader("| Saúde Única")
-        st.write(
-            '<p style="color:#9c9d9f">A abordagem da Saúde Única promove uma visão integrada e multissetorial da saúde, envolvendo humanos, animais e ecossistemas. Reconhece a interdependência desses elementos e mobiliza diversos setores e disciplinas para promover o bem-estar e lidar com ameaças à saúde e aos ecossistemas, incluindo água limpa, segurança alimentar, mudanças climáticas e desenvolvimento sustentável.</p>',
-            unsafe_allow_html=True,
-            )
+    st.subheader("Bem-vindo ao MLens")
+    # Adicionar a imagem ao espaço em branco
+    st.image("MLens.svg")  #  caminho da sua imagem
 
 # Adicione as informações adicionais
 st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/3771047626259544)")
